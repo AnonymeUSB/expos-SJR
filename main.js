78174ff6f0d90fbb435c24f1fb3d7ef0ab5c9f0b
AnonymeUSB <Allyssisation@gmail.com>
2022-09-14 14:05:34 +0000

    js-initialisation2

--- main.js
+++ main.js
@@ -5,17 +5,31 @@ sessionStorage.setItem("diapo", diapo);
 var diapo = sessionStorage.getItem("diapo");
 
 window.addEventListener("keydown", function (event) {
+
     if (event.key == "ArrowRight") {
       
         var diapo = parseInt(sessionStorage.getItem("diapo")) + 1;
         sessionStorage.setItem("diapo", diapo);
-        alert(diapo);
+
+        if (diapo == 3){
+            alert("cc");
+        }
 
     }
     if (event.key == "ArrowLeft"){
         
-        var diapo = parseInt(sessionStorage.getItem("diapo")) + -1;
-        alert(diapo);
+        var diapo = parseInt(sessionStorage.getItem("diapo"));
+        var diapo = diapo - 1;
+        if (diapo < 1){
+            alert("Pas de page avant");
+            var diapo =+ 1;
+        }
+        sessionStorage.setItem("diapo", diapo);
+        
+        if (diapo == 3){
+            alert("cc");
+        }
 
     }
+
   });
