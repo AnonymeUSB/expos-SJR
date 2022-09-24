01e6c41b3c9843dfaae3a346f96ac2ade8329730
AnonymeUSB <Allyssisation@gmail.com>
2022-09-24 13:49:25 +0000

    end

--- main.js
+++ main.js
@@ -3,11 +3,81 @@ var diapo = 1;
 sessionStorage.setItem("diapo", diapo);
 var diapo = sessionStorage.getItem("diapo");
 
+function q1() {
+    
+    var q1 = document.getElementById("q1").value;
+
+    if(q1 == "1"){
+        alert("Réponse incorrecte.")
+    }
+    if(q1 == "2"){
+        alert("Bonne réponse.")
+    }
+    if(q1 == "3"){
+        alert("Réponse incorrecte.")
+    }
+
+}
+
+function q2() {
+    
+    var q2 = document.getElementById("q2").value;
+
+    if(q2 == "1"){
+        alert("Réponse incorrecte.")
+    }
+    if(q2 == "2"){
+        alert("Réponse incorrect.")
+    }
+    if(q2 == "3"){
+        alert("Bonne réponse.")
+    }
+
+}
+
+function q3() {
+    
+    var q3 = document.getElementById("q3").value;
+
+    if(q3 == "1"){
+        alert("Réponse incorrecte.")
+    }
+    if(q3 == "2"){
+        alert("Bonne réponse.")
+    }
+    if(q3 == "3"){
+        alert("Réponse incorrecte.")
+    }
+
+}
+
+function q4() {
+    
+    var q4 = document.getElementById("q4").value;
+
+    if(q4 == "1"){
+        alert("Réponse incorrecte.")
+    }
+    if(q4 == "2"){
+        alert("Bonne réponse.")
+    }
+    if(q4 == "3"){
+        alert("Réponse incorrecte.")
+    }
+
+}
+
 window.addEventListener("keydown", function (event) {
 
     if (event.key == "ArrowRight") {
       
         var diapo = parseInt(sessionStorage.getItem("diapo")) + 1;
+
+        if (diapo > 6){
+            alert("Pas de page après");
+            var diapo =- 1;
+        }
+
         sessionStorage.setItem("diapo", diapo);
 
         if (parseInt(sessionStorage.getItem("diapo")) == 1){
@@ -27,6 +97,21 @@ window.addEventListener("keydown", function (event) {
             document.getElementById("diapo2").style.display = "none";
             document.getElementById("diapo3").style.display = "none";
             document.getElementById("diapo4").style.display = "block";
+        }
+        if (parseInt(sessionStorage.getItem("diapo")) == 5){
+            document.getElementById("diapo1").style.display = "none";
+            document.getElementById("diapo2").style.display = "none";
+            document.getElementById("diapo3").style.display = "none";
+            document.getElementById("diapo4").style.display = "none";
+            document.getElementById("diapo5").style.display = "block";
+        }
+        if (parseInt(sessionStorage.getItem("diapo")) == 6){
+            document.getElementById("diapo1").style.display = "none";
+            document.getElementById("diapo2").style.display = "none";
+            document.getElementById("diapo3").style.display = "none";
+            document.getElementById("diapo4").style.display = "none";
+            document.getElementById("diapo5").style.display = "none";
+            document.getElementById("diapo6").style.display = "block";
         }
 
     }
@@ -57,6 +142,21 @@ window.addEventListener("keydown", function (event) {
             document.getElementById("diapo2").style.display = "none";
             document.getElementById("diapo3").style.display = "none";
             document.getElementById("diapo4").style.display = "block";
+        }
+        if (parseInt(sessionStorage.getItem("diapo")) == 5){
+            document.getElementById("diapo1").style.display = "none";
+            document.getElementById("diapo2").style.display = "none";
+            document.getElementById("diapo3").style.display = "none";
+            document.getElementById("diapo4").style.display = "none";
+            document.getElementById("diapo5").style.display = "block";
+        }
+        if (parseInt(sessionStorage.getItem("diapo")) == 6){
+            document.getElementById("diapo1").style.display = "none";
+            document.getElementById("diapo2").style.display = "none";
+            document.getElementById("diapo3").style.display = "none";
+            document.getElementById("diapo4").style.display = "none";
+            document.getElementById("diapo5").style.display = "none";
+            document.getElementById("diapo6").style.display = "block";
         }
 
     }
