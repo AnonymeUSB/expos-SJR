d442cadb358e71a5a9ff004a149a9b2245584ca7
AnonymeUSB <Allyssisation@gmail.com>
2022-09-24 10:16:26 +0000

    resolve

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-var diapo = 0;
+
+var diapo = 1;
 sessionStorage.setItem("diapo", diapo);
-var guitare = document.getElementById('g');
+var diapo = sessionStorage.getItem("diapo");
 
 window.addEventListener("keydown", function (event) {
 
     if (event.key == "ArrowRight") {
       
-        var diapo = this.sessionStorage.getItem("diapo");
-        var ndiapo = parseInt(diapo) + 1;
-        this.sessionStorage.setItem("diapo", ndiapo);
-        var diapo = this.sessionStorage.getItem("diapo");
+        var diapo = parseInt(sessionStorage.getItem("diapo")) + 1;
+        sessionStorage.setItem("diapo", diapo);
 
         if (parseInt(sessionStorage.getItem("diapo")) == 1){
-            guitare.scrollIntoView({behavior: "smooth"});        }
+            document.getElementById("diapo1").style.display = "block";
+        }
+        if (parseInt(sessionStorage.getItem("diapo")) == 2){
+            document.getElementById("diapo1").style.display = "none";
+        }
 
     }
     if (event.key == "ArrowLeft"){
         
-        var diapo = this.sessionStorage.getItem("diapo");
-        var ndiapo = parseInt(diapo) - 1;
-        this.sessionStorage.setItem("diapo", ndiapo);
-        var diapo = this.sessionStorage.getItem("diapo");
+        var diapo = parseInt(sessionStorage.getItem("diapo"));
+        var diapo = diapo - 1;
+        if (diapo < 1){
+            alert("Pas de page avant");
+            var diapo =+ 1;
+        }
+        sessionStorage.setItem("diapo", diapo);
         
-
+        if (parseInt(sessionStorage.getItem("diapo")) == 1){
+            document.getElementById("diapo1").style.display = "block";
+        }
+        if (parseInt(sessionStorage.getItem("diapo")) == 2){
+            document.getElementById("diapo1").style.display = "none";
+        }
 
     }
 
