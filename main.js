41bd656a2d0e54f0096bf7836892d03050837a49
AnonymeUSB <Allyssisation@gmail.com>
2022-09-24 07:45:24 +0000

    bug

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
-
-var diapo = 1;
+var diapo = 0;
 sessionStorage.setItem("diapo", diapo);
-var diapo = sessionStorage.getItem("diapo");
+var guitare = document.getElementById('g');
 
 window.addEventListener("keydown", function (event) {
 
     if (event.key == "ArrowRight") {
       
-        var diapo = parseInt(sessionStorage.getItem("diapo")) + 1;
-        sessionStorage.setItem("diapo", diapo);
+        var diapo = this.sessionStorage.getItem("diapo");
+        var ndiapo = parseInt(diapo) + 1;
+        this.sessionStorage.setItem("diapo", ndiapo);
+        var diapo = this.sessionStorage.getItem("diapo");
 
         if (parseInt(sessionStorage.getItem("diapo")) == 1){
-            document.getElementById("diapo1").style.display = "block";
-        }
-        if (parseInt(sessionStorage.getItem("diapo")) == 2){
-            document.getElementById("diapo1").style.display = "none";
-        }
+            guitare.scrollIntoView({behavior: "smooth"});        }
 
     }
     if (event.key == "ArrowLeft"){
         
-        var diapo = parseInt(sessionStorage.getItem("diapo"));
-        var diapo = diapo - 1;
-        if (diapo < 1){
-            alert("Pas de page avant");
-            var diapo =+ 1;
-        }
-        sessionStorage.setItem("diapo", diapo);
+        var diapo = this.sessionStorage.getItem("diapo");
+        var ndiapo = parseInt(diapo) - 1;
+        this.sessionStorage.setItem("diapo", ndiapo);
+        var diapo = this.sessionStorage.getItem("diapo");
         
-        if (parseInt(sessionStorage.getItem("diapo")) == 1){
-            document.getElementById("diapo1").style.display = "block";
-        }
-        if (parseInt(sessionStorage.getItem("diapo")) == 2){
-            document.getElementById("diapo1").style.display = "none";
-        }
+
 
     }
 
